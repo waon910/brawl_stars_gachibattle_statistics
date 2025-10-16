5482c5f88534b10a337804b06b128e4ef62bea56
Shunsuke-Iwao <waon11910@gmail.com>
2025-10-16 22:10:53 +0900

    fix

--- sql/-- MySQL：部分一致（contains）.sql
+++ sql/-- MySQL：部分一致（contains）.sql
@@ -1,2 +1,4 @@
 -- MySQL：部分一致（contains）
-SELECT * FROM players WHERE name LIKE CONCAT('%', "CR|Ten", '%');
+SELECT * FROM players WHERE name LIKE CONCAT('%', "see", '%');
+
+-- SELECT * FROM players WHERE tag LIKE CONCAT('%', "#8JYYJ80YJ", '%');
